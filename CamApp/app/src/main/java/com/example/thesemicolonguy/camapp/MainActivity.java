01d871895eb67db5262bfb60c10a3693be7b8782
thesemicolonguy <sarthakgupta259@gmail.com>
2017-10-08 04:17:47 +0530

    added android user interface

---
+++ CamApp/app/src/main/java/com/example/thesemicolonguy/camapp/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.thesemicolonguy.camapp;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSubmit, btnCamera;
+    private ImageView ivImage;
+    private ConnectionDetector cd;
+    private Boolean upflag = false;
+    private Uri selectedImage = null;
+    private Bitmap bitmap, bitmapRotate;
+    private ProgressDialog pDialog;
+    String imagepath = "";
+    String fname;
+    File file;
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)){
+//            //Do something
+//            onClick();
+//        }
+//        return true;
+//    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //        Object initialization
+        cd = new ConnectionDetector(MainActivity.this);
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+
+        cd = new ConnectionDetector(getApplicationContext());
+
+        btnSubmit.setOnClickListener(this);
+        btnCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCamera:
+
+                Intent cameraintent = new Intent(
+                        MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraintent, 101);
+
+                break;
+            case R.id.btnSubmit:
+                if (cd.isConnectingToInternet()) {
+                    if (!upflag) {
+                        Toast.makeText(MainActivity.this, "Image Not Captured..!", Toast.LENGTH_LONG).show();
+                    } else {
+                        saveFile(bitmapRotate, file);
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "No Internet Connection !", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        try {
+            switch (requestCode) {
+                case 101:
+                    if (resultCode == Activity.RESULT_OK) {
+                        if (data != null) {
+                            selectedImage = data.getData(); // the uri of the image taken
+                            if (String.valueOf((Bitmap) data.getExtras().get("data")).equals("null")) {
+                                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                            } else {
+                                bitmap = (Bitmap) data.getExtras().get("data");
+                            }
+                            if (Float.valueOf(getImageOrientation()) >= 0) {
+                                bitmapRotate = rotateImage(bitmap, Float.valueOf(getImageOrientation()));
+                            } else {
+                                bitmapRotate = bitmap;
+                                bitmap.recycle();
+                            }
+
+                            ivImage.setVisibility(View.VISIBLE);
+                            ivImage.setImageBitmap(bitmapRotate);
+
+//                            Saving image to mobile internal memory for sometime
+                            String root = getApplicationContext().getFilesDir().toString();
+                            File myDir = new File(root + "/androidlift");
+                            myDir.mkdirs();
+
+                            Random generator = new Random();
+                            int n = 10000;
+                            n = generator.nextInt(n);
+
+//                            Give the file name that u want
+                            fname = "null" + n + ".jpg";
+
+                            imagepath = root + "/androidlift/" + fname;
+
+//                            Log.i("OOOOOOOOOOOOOOOOOOOO","HEYFUCKER " + imagepath);
+                            file = new File(myDir, fname);
+                            upflag = true;
+                        }
+                    }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Bitmap retVal;
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        retVal = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+
+        return retVal;
+    }
+
+    //    In some mobiles image will get rotate so to correting that this code will help us
+    private int getImageOrientation() {
+        final String[] imageColumns = {MediaStore.Images.Media._ID, MediaStore.Images.ImageColumns.ORIENTATION};
+        final String imageOrderBy = MediaStore.Images.Media._ID + " DESC";
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                imageColumns, null, null, imageOrderBy);
+
+        if (cursor.moveToFirst()) {
+            int orientation = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.ImageColumns.ORIENTATION));
+            System.out.println("orientation===" + orientation);
+            cursor.close();
+            return orientation;
+        } else {
+            return 0;
+        }
+    }
+
+    //    Saving file to the mobile internal memory
+    private void saveFile(Bitmap sourceUri, File destination) {
+        if (destination.exists()) destination.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(destination);
+            sourceUri.compress(Bitmap.CompressFormat.JPEG, 60, out);
+            out.flush();
+            out.close();
+            if (cd.isConnectingToInternet()) {
+                new DoFileUpload().execute();
+            } else {
+                Toast.makeText(MainActivity.this, "No Internet Connection..", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class DoFileUpload extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("wait uploading Image..");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                // Set your file path here
+                FileInputStream fstrm = new FileInputStream(imagepath);
+                // Set your server page url (and the file title/description)
+                HttpFileUpload hfu = new HttpFileUpload("https://hackdata17.herokuapp.com/api/file", "ftitle", "fdescription", fname);
+                upflag = hfu.Send_Now(fstrm);
+            } catch (FileNotFoundException e) {
+                // Error: File not found
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String file_url) {
+            if (pDialog.isShowing()) {
+                pDialog.dismiss();
+            }
+            if (upflag) {
+                Toast.makeText(getApplicationContext(), "Uploading Complete", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Unfortunately file is not Uploaded..", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+}
